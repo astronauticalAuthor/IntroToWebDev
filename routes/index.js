73bc94d4e3156d7ae45f7411b3f7b173b0ec81ba
Katrina <katrina.kerrick@gmail.com>
2016-10-27 19:19:09 -0400

    Server now serves all static files.

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', '/public/html/index.html'));
+  // res.sendFile(path.join(__dirname, '..', '/static/styles/style.css'));
+});
+
+router.get('/about', function(req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public/html/about.html'));
+});
+
+router.get('/rush', function(req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public/html/rush.html'));
+});
+
+router.get('/executives', function(req, res) {
+	res.sendFile(path.join(__dirname, '..', 'public/html/execs.html'));
+});
+
+module.exports = router;
