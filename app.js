2386553ece17f601be39a83b7fe792c5750c59ac
Katrina <katrina.kerrick@gmail.com>
2016-10-31 18:10:22 -0400

    Fixed merge conflicts

--- app.js
+++ app.js
@@ -6,17 +6,12 @@ var port = normalizePort(process.env.PORT || '3000');
 
 // var db = require('./model/db');
 var routes = require('./routes/index');
-<<<<<<< HEAD
 var executives = require('./routes/executives');
-=======
 var posts = require('./routes/posts');
-// var cors = require('cors');
->>>>>>> df1fe82af74b2588ade4fc4cdd47289043008cd0
 
 var app = express();
 
 app.use(express.static('public'));
-<<<<<<< HEAD
 app.use('/', routes);
 app.use('/executiveInfo', executives);
 
@@ -34,13 +29,4 @@ function normalizePort(val) {
     // port number
     return port;
   }
-=======
-
-// app.use(cors());
-app.use('/', routes);
-app.use('/posts', posts);
-
->>>>>>> df1fe82af74b2588ade4fc4cdd47289043008cd0
-
-  return false;
 }
