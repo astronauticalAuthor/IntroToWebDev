e685ca237cf1555b0be2b0e302dcea1acee8d164
Katrina <katrina.kerrick@gmail.com>
2016-10-31 18:54:56 -0400

    Trying to fix executives page

--- public/scripts/execs.js
+++ public/scripts/execs.js
@@ -1,21 +1,15 @@
 function loadExecs() {
 	$.ajax({
-		url: "http://localhost:3000/executiveInfo"
+		url: "https://csse280-term-project-backend.herokuapp.com/executiveInfo"
 	})
     .done(function(data) {
       for (var key in data) {
-        // $('#contentarea').append('<div>' + key + ': ' + data[key] + '</div>');
         var exec = '<div class="execBlock">';
         exec += '<img src="images/' + key + '.jpg" class="execImage" />';
         exec += key + ' - ' + data[key];
         exec += '</div>';
 
-
-
         $('#contentarea').append(exec);
       }
-
-
-
     })
 }
