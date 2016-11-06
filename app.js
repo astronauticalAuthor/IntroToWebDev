d0e142c3d71d287e01c59e15bd84d21be2fd7173
Natasha Tepley <Natasha Tepley>
2016-11-06 17:41:55 -0500

    working on post/post.  Trying to restore my GIT

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 var express = require('express');
+const bodyParser= require('body-parser');
 var path = require('path');
 var http = require('http');
 
 var port = normalizePort(process.env.PORT || '3000');
 
-// var db = require('./model/db');
+var db = require('./model/db');
 var routes = require('./routes/index');
 var executives = require('./routes/executives');
 var posts = require('./routes/posts');
 
 var app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 app.use('/', routes);
-app.use('/posts', posts);
+
 app.use('/executiveInfo', executives);
+
+app.use('/posts', posts);
+app.post('/posts', (req, res) => {
+	  db.save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/blog')
+  })
+});
+
 
 http.createServer(app).listen(port);
 
--- public/scripts/blog.js
+++ public/scripts/blog.js
@@ -2,11 +2,20 @@ var postsDisplayLocation;
 var contentArea;
 var Allposts;
 var isLoggedIn;
+// var theURL= 'http://localhost:3000';
+var theURL = 'https://csse280-term-project-backend.herokuapp.com/posts'
+
+function setUpBlog() {
+    getPosts();
+    addPoster();
+    $("#postSubmit").click(function(){
+        postPost();
+    });
+}
 
 function getPosts() {
     $.ajax({
-        url: 'https://csse280-term-project-backend.herokuapp.com/posts'
-        // url: 'http://localhost:3000/posts'
+        url: theURL + '/posts'
     }).done(function (data){
         displayPosts(data);
     });
@@ -28,13 +37,27 @@ function addPoster() {
     if(!isLoggedIn){
         return;
     }
-    var html = '<form id="login_form">';
+    var html = '<form id="loginForm" action="../posts" method="POST">';
     html += '<p> Post Title:</p>'
-    html += '<input type="text" name="postTitle"/>';
+    html += '<input type="text" name="title"/>';
     html += '<p> Post Body: </p>';
-    html += '<input type="text" name="postBody" class="paragraphInput"/>';
-    html += '</p><input type="button" name="postSubmit" value="Post it!"/></p>';
+    html += '<input type="text" name="body" class="paragraphInput"/>';
+    html += '<p><button type="submit">Post it!</button></p>';
     html += '</form>';
 
     $("#contentarea").append(html);
 }
+
+// function postPost() {
+//     var aForm = $('#loginForm');
+//     var aTitle = aForm.title;
+//     var aBody = aForm.body;
+//     $.ajax({
+//         method: "POST",
+//         url: theURL + '/posts',
+//         data: { title: aTitle, body: aBody}
+//     }).done(function (){
+//         console.log("It thinks it finished?");
+//         window.location.replace("/blog");
+//     });
+// }
