5d7c65bb3ad43480c471c79ddbae64a75e121baf
Natasha Tepley <Natasha Tepley>
2016-11-06 11:00:27 -0500

    added a bare 'form' to the blog page.   always appears, and doesnt post to db yet

--- public/scripts/blog.js
+++ public/scripts/blog.js
@@ -1,42 +1,40 @@
 var postsDisplayLocation;
 var contentArea;
 var Allposts;
+var isLoggedIn;
 
 function getPosts() {
     $.ajax({
         url: 'https://csse280-term-project-backend.herokuapp.com/posts'
+        // url: 'http://localhost:3000/posts'
     }).done(function (data){
         displayPosts(data);
     });
 }
 
-// // save contact to update in browser storage and go to update page
-// function contactRowClickHandler(contact) {
-//     var error = false;
-//     function contactWithID(thiscontact) {
-//         return thiscontact._id === contact._id;
-//     }
-//     var contactToUpdate = Allcontacts.filter(contactWithID)[0];
-//     try {
-//         var contactToUpdateString = JSON.stringify(contactToUpdate);
-//         sessionStorage.setItem("contactToUpdate", contactToUpdateString);
-//     } catch (e) {
-//         alert("Error when writing to Session Storage " + e);
-//         error = true;
-//     }
-//     if (!error) {
-//         window.location = "update.html";
-//         return false;
-//     }
-// }
-
 function displayPosts(posts) {
     for (var x = 0; x < posts.length; x++) {
-        var html = '<div class="blogPosts">';
+        var html = '<div class="post">';
         html += 'Title: ' + posts[x].title + '<br />';
         html += posts[x].body + '<br />';
         html += posts[x].datePosted + '</div><hr />';
 
-        $("#contentarea").append(html);
+        $("#blogPosts").append(html);
     }
+}
+
+function addPoster() {
+    isLoggedIn = true;
+    if(!isLoggedIn){
+        return;
+    }
+    var html = '<form id="login_form">';
+    html += '<p> Post Title:</p>'
+    html += '<input type="text" name="postTitle"/>';
+    html += '<p> Post Body: </p>';
+    html += '<input type="text" name="postBody" class="paragraphInput"/>';
+    html += '</p><input type="button" name="postSubmit" value="Post it!"/></p>';
+    html += '</form>';
+
+    $("#contentarea").append(html);
 }
