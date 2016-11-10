ed68f89d1527f4eaffbea008a967b4759b67bc0f
Katrina <katrina.kerrick@gmail.com>
2016-11-09 21:08:31 -0500

    Posts can now update and delete

--- public/scripts/navigation.js
+++ public/scripts/navigation.js
@@ -1,9 +1,9 @@
-var isLoggedIn = false;
 var publicLinks = [["/", "Home"], ["/about", "About"], ["/rush", "Rush"], ["/executives","Executives"],["/blog","Blog"]];
 var privateLinks = [["/schedule", "Schedule"],["/trees","Trees"]];
 
 function mainNavigation() {
-	var url = 'https://csse280-term-project-backend.herokuapp.com';
+	// var url = 'https://csse280-term-project-backend.herokuapp.com';
+	var url = 'http://localhost:3000';
 
 	$.ajax({
 		url: url + '/login/login/checkLoggedIn',
